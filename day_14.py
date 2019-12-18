ea5e68d3de905bf6fbe99d1dd7c0aa6bed681d2b
Matthiti_13 <mj.roelink@outlook.com>
2019-12-18 10:11:44 +0100

    Finished day 14

--- day_14.py
+++ day_14.py
@@ -1,32 +1,70 @@
+import math
+
 ## PART 1 ##
-'''
-input = """10 ORE => 10 A
-1 ORE => 1 B
-7 A, 1 B => 1 C
-7 A, 1 C => 1 D
-7 A, 1 D => 1 E
-7 A, 1 E => 1 FUEL"""
-'''
 
-input = """9 ORE => 2 A
-8 ORE => 3 B
-7 ORE => 5 C
-3 A, 4 B => 1 AB
-5 B, 7 C => 1 BC
-4 C, 1 A => 1 CA
-2 AB, 3 BC, 4 CA => 1 FUEL"""
-
-'''
-input = """157 ORE => 5 NZVS
-165 ORE => 6 DCFZ
-44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-179 ORE => 7 PSHF
-177 ORE => 5 HKGWZ
-7 DCFZ, 7 PSHF => 2 XJWVT
-165 ORE => 2 GPVTF
-3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"""
-'''
+input = """14 LQGXD, 6 TDLQ => 9 VGLV
+1 WBQF, 2 JZKMJ => 5 TRSK
+5 MGHZ, 5 ZLDQF => 8 HMVG
+1 JWQH, 1 QFBC, 2 ZXVNM => 8 JFJZH
+8 QTPX, 8 LDLWS => 6 NVZPS
+2 QPWF, 1 PRWSM => 5 WHWF
+1 QPWF, 8 LDLWS => 5 LZBQ
+127 ORE => 1 MDPJB
+4 WHWF => 4 KQHW
+1 QBCKX, 3 TMTH => 4 WLFTZ
+15 NPMPT, 4 TMTH => 6 QFBC
+12 MDPJB => 9 PRWSM
+5 QXHFH, 3 LCDVR, 24 MWFP, 1 MSFV, 1 BPDJL, 3 LQGXD, 2 DVGW => 2 KCPSH
+6 FPZXN, 1 FQSK, 3 TMTH => 1 FBHW
+25 PRWSM => 1 MGHZ
+6 XWKXC, 5 TMTH, 1 PZTGX => 6 NTQZ
+3 BPDJL, 3 DJWCL, 2 JZKMJ => 7 MWFP
+5 JFJZH => 3 DJWCL
+22 WRNJ, 12 TRSK => 5 TBGJC
+3 HKWP => 1 PDRN
+3 JWQH => 5 JZKMJ
+4 WBQF => 2 BJNS
+1 GNBQ => 9 FQSK
+8 HMVG, 1 HQHD => 5 NJFNC
+7 QBCKX, 1 FQSK => 9 NDCQ
+3 XWKXC, 7 QFBC, 3 GPFRS, 2 LPQZ, 2 LQGXD, 20 LZKM, 1 QRTH => 8 TDTKT
+1 QTPX => 3 LPQZ
+2 QGVQC, 14 LDLWS => 1 NPMPT
+1 QRTH, 7 BPDJL => 7 XWKXC
+9 WLFTZ, 8 TDLQ => 6 GKPK
+4 GNBQ => 3 QXHFH
+3 TBGJC, 1 LPQZ => 3 DVGW
+3 NDCQ, 1 KGZT => 7 FPZXN
+36 WLFTZ, 1 KCPSH, 1 GKPK, 1 TDTKT, 3 CSPFK, 27 JZKMJ, 5 VGLV, 39 XWKXC => 1 FUEL
+115 ORE => 7 QGVQC
+21 NTQZ, 11 HQHD, 33 JFJZH, 3 NJFNC, 3 MSFV, 1 TRSK, 7 WRNJ => 9 CSPFK
+3 DVGW => 4 TDLQ
+5 FPZXN => 6 WRNJ
+10 TSDLM, 17 XDKP, 3 PDRN => 2 HQHD
+1 PCWS => 3 PZTGX
+2 QXHFH => 5 JWQH
+17 KQHW => 2 WBQF
+139 ORE => 5 LDLWS
+3 TSDLM => 9 KGZT
+16 NPMPT => 3 QTPX
+3 DVGW, 5 KVFMS, 3 WLFTZ => 6 GPFRS
+1 PZTGX, 2 LCDVR, 13 TBGJC => 6 LZKM
+5 ZXVNM, 2 QXHFH => 4 MSFV
+4 XDKP, 7 FBHW, 2 PCWS => 3 LCDVR
+3 TRSK => 7 KVFMS
+10 LDLWS => 9 TMTH
+2 TBGJC => 6 LQGXD
+2 TRSK => 6 ZXVNM
+4 KQHW, 1 NVZPS => 8 ZLDQF
+2 LZBQ => 4 QBCKX
+7 QBCKX => 6 TSDLM
+152 ORE => 3 QPWF
+2 TSDLM, 8 WHWF => 3 HKWP
+19 FQSK => 8 QRTH
+19 QTPX => 3 GNBQ
+4 PDRN, 12 HKWP, 4 PCWS => 3 XDKP
+6 LZBQ, 19 BJNS => 5 BPDJL
+5 HKWP, 6 NVZPS => 3 PCWS"""
 
 
 class Chemical(object):
@@ -41,10 +79,10 @@ class Chemical(object):
 		return str(self)
 		
 	def __eq__(self, other: 'Chemical') -> bool:
-		return self.name == other.name
+		return self.name == other.name and self.amount == other.amount
 		
 	def __hash__(self) -> int:
-		return hash(self.name)
+		return hash((self.name, self.amount))
 
 
 class Reaction(object):
@@ -81,8 +119,7 @@ def main(input: str=input) -> int:
 	return ore_to_produce_chemical(Chemical("FUEL", 1), reactions, {})
 
 
-def ore_to_produce_chemical(chemical: (str, int), reactions: [Reaction], stack: {Chemical: int}) -> int:
-	print(stack)
+def ore_to_produce_chemical(chemical: (str, int), reactions: [Reaction], stack: {str: int}) -> int:
 	if chemical.name == 'ORE':
 		return chemical.amount
 
@@ -91,29 +128,26 @@ def ore_to_produce_chemical(chemical: (str, int), reactions: [Reaction], stack: 
 	# r.out_chemical.amount = 5
 	r = find_out_reaction(chemical, reactions)
 	
-	multiplier = 1
-	while chemical.amount > r.out_chemical.amount * multiplier:
-		multiplier += 1
+	multiplier = math.ceil(chemical.amount / r.out_chemical.amount)
 			
 	diff = r.out_chemical.amount * multiplier - chemical.amount
 	if diff > 0:
-		if chemical in stack:
-			stack[chemical] += diff
+		if chemical.name in stack:
+			stack[chemical.name] += diff
 		else:
-			stack[chemical] = diff
+			stack[chemical.name] = diff
 
 	total = 0
 	for i in r.in_chemicals:
 		amount_needed = i.amount * multiplier
-		if i in stack:
-			if stack[i] >= amount_needed:
-				stack[i] -= amount_needed
+		if i.name in stack:
+			if stack[i.name] >= amount_needed:
+				stack[i.name] -= amount_needed
 				continue
 			else:
-				amount_needed -= stack[i]
-				stack[i] = 0
-		i.amount = amount_needed
-		total += ore_to_produce_chemical(i, reactions, stack)
+				amount_needed -= stack[i.name]
+				stack[i.name] = 0
+		total += ore_to_produce_chemical(Chemical(i.name, amount_needed), reactions, stack)
 	return total
 
 
@@ -123,3 +157,10 @@ def find_out_reaction(chemical: (str, int), reactions: [Reaction]) -> Reaction:
 			return r
 	return None  # Should never occur
 
+
+## PART 2 ##
+# Done by adjusting and trying fuel_amount #
+def main_2(input: str = input) -> int:
+	reactions = parse_input(input)
+	fuel_amount = 1120408
+	return ore_to_produce_chemical(Chemical("FUEL", fuel_amount), reactions, {})
